7d8d85de59dd05ad1536222f9138ef0b4e33b2b7
李嘉博 <1658446745@qq.com>
2021-11-03 15:59:41 +0800

    Add files via upload

---
+++ src/CryptoDemoZero/js/load-wallpaper.js
@@ -0,0 +1,3 @@
+var wallpaper=localStorage.getItem("wallpaper");
+document.getElementById("body").style.backgroundImage="url("+wallpaper+")";
+document.getElementById("content").style.backgroundImage="url("+wallpaper+")";
